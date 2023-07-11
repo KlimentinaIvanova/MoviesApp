0aaed08825589585d7e239ef781d10ef754dbd52
Klimentina Ivanova <klimentina1988@gmail.com>
2023-07-11 17:29:35 +0300

    Adding Search Box and App.css

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React,{useState,useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox'
+
+function App() {
+
+  const [movies,setMovies]=useState([])
+  const [searchValue, setSearchValue]=useState('')
+
+  return (
+    <div className="container-fluid movie-app">
+      <div className='row d-flex aling-items-center mt-4 mb-4'>
+        <MovieListHeading heading="Movies"/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+     <div className="row">
+      <MovieList
+      movies={movies}
+      />
+     </div>
+    </div>
+  );
+}
+
+export default App;
